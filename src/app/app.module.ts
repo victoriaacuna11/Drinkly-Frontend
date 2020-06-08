723a78585a980a50de84aa244e88feffe6f1c62b
Victoria Acuna <victoria.acuna@correo.unimet.edu.ve>
2020-06-07 21:34:49 -0400

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,5 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ListComponent } from './components/list/list.component';
-import { BarsListComponent } from './components/bars/bars-list/bars-list.component';
-import { DrinksListComponent } from './components/drinks/drinks-list/drinks-list.component';
-import { DrinksFilterComponent } from './components/drinks/drinks-filter/drinks-filter.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { PruebaComponent } from './components/prueba/prueba.component';
-import { PruebaNewComponent } from './components/prueba-new/prueba-new.component';
-import { HttpClientModule } from '@angular/common/http';
-import { PruebaEditComponent } from './components/prueba-edit/prueba-edit.component';
-import { AdminHeaderComponent } from './components/admin/admin-header/admin-header.component';
-import { AddIngredientComponent } from './components/admin/ingredient/add-ingredient/add-ingredient.component';
-import { ListIngredientComponent } from './components/admin/ingredient/list-ingredient/list-ingredient.component';
-import { EditIngredientComponent } from './components/admin/ingredient/edit-ingredient/edit-ingredient.component';
 
-=======
+import { EditIngredientComponent } from './components/admin/ingredient/edit-ingredient/edit-ingredient.component';
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
@@ -39,7 +19,6 @@ import { AdminHeaderComponent } from "./components/admin/admin-header/admin-head
 import { AddIngredientComponent } from "./components/admin/ingredient/add-ingredient/add-ingredient.component";
 import { ListIngredientComponent } from "./components/admin/ingredient/list-ingredient/list-ingredient.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
->>>>>>> 2e5ab18da85241a7847712b7a9212e7e79352746
 
 @NgModule({
   declarations: [
@@ -56,11 +35,8 @@ import { NotFoundComponent } from "./components/not-found/not-found.component";
     AdminHeaderComponent,
     AddIngredientComponent,
     ListIngredientComponent,
-<<<<<<< HEAD
-    EditIngredientComponent
-=======
-    NotFoundComponent,
->>>>>>> 2e5ab18da85241a7847712b7a9212e7e79352746
+    EditIngredientComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -72,4 +48,4 @@ import { NotFoundComponent } from "./components/not-found/not-found.component";
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
