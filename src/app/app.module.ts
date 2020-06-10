e31703c8a6d82d365372b4771fd322694f67e49d
elenamm12 <elena.7moya@gmail.com>
2020-06-09 20:12:02 -0400

    merge sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
 import { AdminAuthGuard } from "./guards/adminAuth.guard";
 import { AuthGuard } from "./guards/auth.guard";
 import { AuthService } from "./services/auth.service";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { HeaderComponent } from "./components/header/header.component";
-import { ListComponent } from "./components/list/list.component";
-import { BarsListComponent } from "./components/bars/bars-list/bars-list.component";
-import { DrinksListComponent } from "./components/drinks/drinks-list/drinks-list.component";
-import { DrinksFilterComponent } from "./components/drinks/drinks-filter/drinks-filter.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { PruebaComponent } from "./components/prueba/prueba.component";
-import { PruebaNewComponent } from "./components/prueba-new/prueba-new.component";
-import { HttpClientModule } from "@angular/common/http";
-import { PruebaEditComponent } from "./components/prueba-edit/prueba-edit.component";
-import { AdminHeaderComponent } from "./components/admin/admin-header/admin-header.component";
 import { AddIngredientComponent } from "./components/admin/ingredient/add-ingredient/add-ingredient.component";
 import { ListIngredientComponent } from "./components/admin/ingredient/list-ingredient/list-ingredient.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
@@ -27,31 +10,25 @@ import { ProfileComponent } from "./components/profile/profile.component";
 import { ValidateService } from "./services/validate.service";
 import { EditIngredientComponent } from "./components/admin/ingredient/edit-ingredient/edit-ingredient.component";
 import { AddBarComponent } from './components/admin/bar/add-bar/add-bar.component';
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AdminHeaderComponent } from './components/admin/admin-header/admin-header.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ListComponent } from './components/list/list.component';
 import { BarsListComponent } from './components/bars/bars-list/bars-list.component';
 import { DrinksListComponent } from './components/drinks/drinks-list/drinks-list.component';
 import { DrinksFilterComponent } from './components/drinks/drinks-filter/drinks-filter.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-<<<<<<< HEAD
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { PruebaNewComponent } from './components/prueba-new/prueba-new.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PruebaEditComponent } from './components/prueba-edit/prueba-edit.component';
 import { FilterChildComponent } from './components/drinks/filter-child/filter-child.component';
 import { FilterParentComponent } from './components/drinks/filter-parent/filter-parent.component';
-=======
-import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
->>>>>>> sidebar
-
->>>>>>> fix_filter
 
 @NgModule({
   declarations: [
@@ -62,11 +39,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     BarsListComponent,
     DrinksListComponent,
     DrinksFilterComponent,
-<<<<<<< HEAD
     PruebaComponent,
     PruebaNewComponent,
     PruebaEditComponent,
-<<<<<<< HEAD
     AdminHeaderComponent,
     AddIngredientComponent,
     ListIngredientComponent,
@@ -76,14 +51,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     RegisterComponent,
     ProfileComponent,
     AddBarComponent,
-=======
     FilterChildComponent,
-    FilterParentComponent
->>>>>>> fix_filter
-=======
+    FilterParentComponent,
     NotFoundComponent,
-    SidebarComponent
->>>>>>> sidebar
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
