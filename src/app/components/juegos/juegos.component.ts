9dc237362fcc09858728e16b9c0267a4d07ae6af
elenamm12 <elena.7moya@gmail.com>
2020-06-26 00:45:25 -0400

    juegos y recetas estaticas

--- src/app/components/juegos/juegos.component.ts
+++ src/app/components/juegos/juegos.component.ts
@@ -9,8 +9,19 @@ export class JuegosComponent implements OnInit {
 
   tridentisOpen: boolean = false;
   chupisisOpen: boolean = false;
+  vorisOpen: boolean = false;
   temas: string[]=['Peliculas','Famosos','Series','Animales','Marcas de Carro','Equipos de Futbol','Capitales','Marcas de Zapato'];
+  retos: string[]=['Besa a alguien del grupo', 
+                  'Escribele a tu ex y dile que lo extrañas', 
+                  'Llama a tu crush',
+                  'Monta una foto fea en tus stories'];
+  verdades: string[]=['Has cometido un delito?', 
+                    'Te gusta alguien en esta mesa?', 
+                    'Que es lo mas cochino que has hecho en tu vida?',
+                    'A quien matarias de tus exes?'];
   elegido: string;
+  verdad: string;
+  reto: string;
 
   constructor() { }
 
@@ -19,6 +30,14 @@ export class JuegosComponent implements OnInit {
 
   elegirTema() {
     this.elegido = this.temas[Math.floor(Math.random() * this.temas.length)]
+  }
+
+  elegirVerdad() {
+    this.verdad = this.verdades[Math.floor(Math.random() * this.verdades.length)]
+  }
+
+  elegirReto() {
+    this.reto = this.retos[Math.floor(Math.random() * this.retos.length)]
   }
 
 }
