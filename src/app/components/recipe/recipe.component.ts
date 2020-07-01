2b85d130e62c4d4e54a30d42faac43d0b203c509
Victoria Acuna <victoria.acuna@correo.unimet.edu.ve>
2020-07-01 09:42:58 -0400

    recipe loca

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -5,6 +5,7 @@ import { DrinkService } from 'src/app/services/drink.service';
 import { drink } from 'src/app/models/drink';
 import { IngredientService } from "src/app/services/ingredient.service";
 import { ingredient } from "src/app/models/ingredient";
+import { database } from 'firebase';
 
 @Component({
   selector: 'app-recipe',
@@ -12,10 +13,11 @@ import { ingredient } from "src/app/models/ingredient";
   styleUrls: ['./recipe.component.scss']
 })
 export class RecipeComponent implements OnInit {
-  load: Boolean = true;
+  
+  loading: Boolean = true;
   sidebar: Boolean;
   receta: drink;
-  ingredients: String[];
+  ingredients: ingredient[];
   ingreAux: ingredient;
 
 
@@ -24,20 +26,41 @@ export class RecipeComponent implements OnInit {
 
   ngOnInit() {
     this.getReceta();
-    this.load = false;
   }
 
-  getIngredients() {
-    var i: number;
-    for(i=0; i<this.receta.ingredients.length; i++){
-      this.service_ing.getIngredient(this.receta.ingredients[i]).subscribe( (r:any) => {
-        this.ingreAux = {...r.data};
-        this.receta.ingredients[i] = this.ingreAux.name;
-        console.log(this.receta.ingredients[i]);
-        console.log(this.load)
-      })
+  // getIngredients() {
+  //   var i: number;
+  //   for(i=0; i<this.receta.ingredients.length; i++){
+  //     this.service_ing.getIngredient(this.receta.ingredients[i]).subscribe( (r:any) => {
+  //       this.ingreAux = {...r.data};
+  //       this.receta.ingredients[i] = this.ingreAux.name;
+  //       console.log(this.receta.ingredients[i]);
+  //       console.log(this.load)
+  //     })
+  //   }
+  //   console.log(this.load)
+  // }
+
+  getIngredientName(id: String){
+    let name = '';
+    let found = false;
+    let i=0;
+    while(!found && i<this.ingredients.length){
+       if(this.ingredients[i]._id==id){
+         found = true;
+         name = this.ingredients[i].name.toString();
+       }
+      i=i+1;
     }
-    console.log(this.load)
+    return name
+    
+  }
+
+  getIngredients(){
+    this.service_ing.getIngredients().subscribe((res:any) => {
+      this.ingredients=[...res.data];
+      this.loading=false;
+    })
   }
 
   getMessage($event){
@@ -48,7 +71,11 @@ export class RecipeComponent implements OnInit {
     const id = this.rout.snapshot.paramMap.get('id');
     this.service.getDrink(id).subscribe( (r:any) => {
       this.receta = {...r.data};
-      this.getIngredients()
+      // this.getIngredients();
+      console.log('Drink...');
+      console.log(this.receta);
+      console.log(this.receta.ingredients.length)
+      this.getIngredients();
     })
     
   }
